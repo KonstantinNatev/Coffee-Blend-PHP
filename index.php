e6cc8bfe73d766f2ec30d0195d47568fc557b9ef
Konstantin-Natev <76740873+KonstantinNatev@users.noreply.github.com>
2024-11-05 17:17:02 +0200

    feat: creat new table and create request to the db for new order

--- index.php
+++ index.php
@@ -283,12 +283,12 @@
 			<?php foreach($allProducts as $product) : ?>
 			<div class="col-md-3">
 				<div class="menu-entry">
-						<a href="products/product-single.php?id<?php echo $product->id; ?>" class="img" style="background-image: url(images/<?php echo $product->image; ?>);"></a>
+						<a targe="_blank" href="products/product-single.php?id=<?php echo $product->id; ?>" class="img" style="background-image: url(images/<?php echo $product->image; ?>);"></a>
 						<div class="text text-center pt-4">
 							<h3><a href="#"><?php echo $product->name; ?></a></h3>
 							<p><?php echo $product->description; ?></p>
 							<p class="price"><span><?php echo $product->price; ?></span></p>
-							<p><a href="products/product-single.php?id<?php echo $product->id; ?>" class="btn btn-primary btn-outline-primary">Show</a></p>
+							<p><a targe="_blank" href="products/product-single.php?id=<?php echo $product->id; ?>" class="btn btn-primary btn-outline-primary">Show</a></p>
 						</div>
 					</div>
 			</div>
